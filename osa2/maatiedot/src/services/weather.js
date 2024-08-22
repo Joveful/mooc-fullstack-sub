6fd47fe5946402ea9d5c8abfac1785a178703fbc
jvehk@LAPTOP-J81G0J0F <j.vehkakoski@gmail.com>
2024-08-22 12:30:31 +0300

    api fix

--- osa2/maatiedot/src/services/weather.js
+++ osa2/maatiedot/src/services/weather.js
@@ -1,4 +1,5 @@
-import axios from "axios";
+import axios from "axios"
+const apikey = import.meta.env.VITE_SOME_KEY
 
 const getCountries = () => {
     const request = axios.get('https://studies.cs.helsinki.fi/restcountries/api/all')
@@ -7,7 +8,6 @@ const getCountries = () => {
 
 const getWeather = country => {
     const latlong = country.latlng
-    const apikey = '6f610a6b50e9ad391900ec7f4ad79e53'
     const request = axios.get(`https://api.openweathermap.org/data/2.5/forecast?lat=${latlong[0]}&lon=${latlong[1]}&appid=${apikey}`)
     return request.then(response => response.data)
 }
