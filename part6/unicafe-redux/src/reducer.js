542086b5177acdb4eb036bc0db4cfb60e595d9b5
Jori Vehkakoski <j.vehkakoski@gmail.com>
2025-03-04 01:43:01 +0200

    jest tests

--- part6/unicafe-redux/src/reducer.js
+++ part6/unicafe-redux/src/reducer.js
@@ -8,14 +8,11 @@ const counterReducer = (state = initialState, action) => {
   console.log(action)
   switch (action.type) {
     case 'GOOD':
-      state.good += 1
-      return state
+      return { ...state, good: state.good + 1 }
     case 'OK':
-      state.ok += 1
-      return state
+      return { ...state, ok: state.ok + 1 }
     case 'BAD':
-      state.bad += 1
-      return state
+      return { ...state, bad: state.bad + 1 }
     case 'ZERO':
       return { good: 0, ok: 0, bad: 0 }
     default: return state
