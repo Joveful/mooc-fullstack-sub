db7f71eb78b31f76e9e8d126d4136be9244b2cd0
Jori Vehkakoski <j.vehkakoski@gmail.com>
2025-05-11 00:42:18 +0300

    smol fixes

--- part9/ts-ex/exerciseCalculator.ts
+++ part9/ts-ex/exerciseCalculator.ts
@@ -8,15 +8,15 @@ interface Result {
     average: number
 }
 
-const calculateExercises = (days: number[], target: number): Result => {
+const calculateExercises = (target: number, days: number[]): Result => {
     const sum = days.reduce((partialSum, d) => partialSum + d, 0)
     const average = sum / days.length
     const trainingDays = days.filter((day) => day > 0).length
 
     var rating
-    if (average <= 5) {
+    if (average <= 1) {
         rating = 1
-    } else if (average > 5 && average < 10) {
+    } else if (average > 1 && average < 2) {
         rating = 2
     } else {
         rating = 3
@@ -29,7 +29,7 @@ const calculateExercises = (days: number[], target: number): Result => {
     return {
         periodLength: days.length,
         trainingDays: trainingDays,
-        success: (rating >= target) ? true : false,
+        success: (average >= target) ? true : false,
         rating: rating,
         ratingDescription: descriptions[rating - 1],
         target: target,
@@ -37,4 +37,4 @@ const calculateExercises = (days: number[], target: number): Result => {
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2))
+console.log(calculateExercises(2, [3, 0, 2, 4.5, 0, 3, 1]))
