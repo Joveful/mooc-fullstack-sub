3931686c68aea8295c1807ab07c0c046e6648fb3
Jori Vehkakoski <j.vehkakoski@gmail.com>
2025-05-15 14:00:55 +0300

    exercise calculator endpoint

--- part9/ts-ex/exerciseCalculator.ts
+++ part9/ts-ex/exerciseCalculator.ts
@@ -8,7 +8,7 @@ interface Result {
     average: number
 }
 
-const calculateExercises = (target: number, days: number[]): Result => {
+export const calculateExercises = (target: number, days: number[]): Result => {
     const sum = days.reduce((partialSum, d) => partialSum + d, 0);
     const average = sum / days.length;
     const trainingDays = days.filter((day) => day > 0).length;
@@ -37,24 +37,26 @@ const calculateExercises = (target: number, days: number[]): Result => {
     };
 };
 
-if (process.argv.length < 3) {
-    throw new Error(" Give at least two arguments");
-};
+if (require.main === module) {
+    if (process.argv.length < 3) {
+        throw new Error(" Give at least two arguments");
+    };
 
-for (let i = 2; i < process.argv.length; i++) {
-    if (isNaN(Number(process.argv[i]))) {
-        throw new Error("Function arguments must be numbers");
+    for (let i = 2; i < process.argv.length; i++) {
+        if (isNaN(Number(process.argv[i]))) {
+            throw new Error("Function arguments must be numbers");
+        }
     }
-}
 
-try {
-    const target: number = Number(process.argv[2]);
-    const days: number[] = process.argv.slice(3).map(Number);
-    console.log(calculateExercises(target, days));
-} catch (error: unknown) {
-    let errorMessage = "Something went wrong.";
-    if (error instanceof Error) {
-        errorMessage += " Error: " + error.message;
+    try {
+        const target: number = Number(process.argv[2]);
+        const days: number[] = process.argv.slice(3).map(Number);
+        console.log(calculateExercises(target, days));
+    } catch (error: unknown) {
+        let errorMessage = "Something went wrong.";
+        if (error instanceof Error) {
+            errorMessage += " Error: " + error.message;
+        }
+        console.log(errorMessage);
     }
-    console.log(errorMessage);
 }
--- part9/ts-ex/index.ts
+++ part9/ts-ex/index.ts
@@ -1,24 +1,43 @@
 import express from 'express';
 import { calculateBmi } from './bmiCalculator';
+import { calculateExercises } from './exerciseCalculator';
 
 const app = express();
 
-app.get("/hello", (_req, res) => {
+app.get('/hello', (_req, res) => {
     res.send("Hello Full Stack!");
 });
 
-app.get("/bmi", (req, res) => {
+app.get('/bmi', (req, res) => {
     const h = Number(req.query.height);
     const w = Number(req.query.weight);
     if (isNaN(h) || isNaN(w)) {
-        res.send({ error: "malformatted parameters" });
+        res.status(400).send({ error: "malformatted parameters" });
     } else {
         const bmi = calculateBmi(h, w);
         res.send({ weight: w, height: h, bmi: bmi });
     }
 });
 
-const PORT = 3000;
+app.post('/exercises', express.json(), (req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const { daily_exercises, target } = req.body;
+    console.log(target)
+
+    if (daily_exercises === undefined || target === undefined) {
+        res.status(400).send({ "error": "missing parameters" });
+    }
+
+    const notANumber = (e: number) => isNaN(e);
+    if (daily_exercises.some(notANumber) || isNaN(target)) {
+        res.status(400).send({ "error": "malformatted parameters" });
+    }
+    const result = calculateExercises(target, daily_exercises);
+    res.send(result);
+});
+
+const PORT = 3003;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
