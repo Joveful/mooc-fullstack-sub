7eb67d04d528d431f2036d472bd7ef8a4c26f92e
Jori Vehkakoski <j.vehkakoski@gmail.com>
2025-05-16 02:52:55 +0300

    post patient

--- part9/patientor-be/src/routes/patients.ts
+++ part9/patientor-be/src/routes/patients.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import { v1 as uuid } from 'uuid';
 import { Response } from 'express';
 import patientsData from '../../data/patients';
 import { NonSensitivePatients } from '../types';
@@ -10,6 +11,16 @@ router.get('/', (_req, res: Response<NonSensitivePatients[]>) => {
     id, name, dateOfBirth, gender, occupation
   }));
   res.send(patientsToReturn);
+});
+
+router.post('/', (req, res) => {
+  /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+  const { name, occupation, dateOfBirth, ssn, gender } = req.body;
+  const id = uuid();
+  const patient = { id, name, ssn, dateOfBirth, gender, occupation };
+
+  patientsData.push(patient);
+  res.send(patient);
 });
 
 export default router;
