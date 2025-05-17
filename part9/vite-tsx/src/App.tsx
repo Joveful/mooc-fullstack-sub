41930828160ba11a86a0def1cf13282bb5921bed
Jori Vehkakoski <j.vehkakoski@gmail.com>
2025-05-17 12:37:41 +0300

    more interfaces and type narrowing

--- part9/vite-tsx/src/App.tsx
+++ part9/vite-tsx/src/App.tsx
@@ -1,13 +1,28 @@
 import type { ReactNode } from "react"
 
-interface CourseProps {
-  name: string,
-  exerciseCount: number
+
+interface CoursePartBase {
+  name: string;
+  exerciseCount: number;
 }
 
-interface Courses {
-  courses: CourseProps[]
+interface CoursePartBasic extends CoursePartBase {
+  description: string;
+  kind: "basic"
 }
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartBase {
+  description: string;
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
 
 const Header = ({ name }: { name: string }) => {
   return (
@@ -15,12 +30,35 @@ const Header = ({ name }: { name: string }) => {
   )
 }
 
-const Content = ({ courses }: Courses) => {
+const Content = ({ courses }: { courses: CoursePart[] }) => {
   return (
     <div>
-      {courses.map((c: CourseProps): ReactNode =>
-        <p key={c.name}>{c.name} {c.exerciseCount}</p>
-      )}
+      {courses.map((c: CoursePart): ReactNode => {
+        switch (c.kind) {
+          case 'basic':
+            return (
+              <div>
+                <p><b>{c.name} {c.exerciseCount}</b><br />
+                  <i>{c.description}</i></p>
+              </div>
+            )
+          case 'group':
+            return (
+              <div>
+                <p><b>{c.name} {c.exerciseCount}</b><br />
+                  project exercises {c.groupProjectCount}</p>
+              </div>
+            )
+          case 'background':
+            return (
+              <div>
+                <p><b>{c.name} {c.exerciseCount}</b><br />
+                  submit to {c.backgroundMaterial}</p>
+              </div>)
+          default:
+            break;
+        }
+      })}
     </div>
   )
 }
@@ -35,20 +73,40 @@ const Total = ({ totalExercises }: { totalExercises: number }) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
-      exerciseCount: 10
+      exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic"
     },
     {
       name: "Using props to pass data",
-      exerciseCount: 7
+      exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group"
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic"
     },
     {
       name: "Deeper type usage",
-      exerciseCount: 14
-    }
+      exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+      kind: "background"
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
   ];
+
 
   const totalExercises = courseParts.reduce((sum, part) => sum + part.exerciseCount, 0);
 
