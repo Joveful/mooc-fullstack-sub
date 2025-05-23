9c52e163379e2445145d18a266a6078d2cb1b419
Jori Vehkakoski <j.vehkakoski@gmail.com>
2025-05-23 14:19:39 +0300

    diagnoses from backend

--- part9/patientor/src/components/PatientPage/index.tsx
+++ part9/patientor/src/components/PatientPage/index.tsx
@@ -1,16 +1,21 @@
 import { useParams } from "react-router-dom";
-import { Entry, Patient } from "../../types";
+import { Entry, Patient, Diagnosis } from "../../types";
 import patientService from "../../services/patients";
+import diagnosisService from "../../services/diagnoses";
 import { useEffect, useState } from "react";
 
 const PatientPage = () => {
   const id = String(useParams().id);
   const [patient, setPatient] = useState<Patient>();
+  const [diagnoses, setDiagnoses] = useState<Diagnosis[]>([]);
 
   useEffect(() => {
     const fetchPatient = async (id: string) => {
       const p = await patientService.getPatientById(id);
       setPatient(p);
+
+      const d = await diagnosisService.getAll();
+      setDiagnoses(d);
     };
     void fetchPatient(id);
   }, [id]);
@@ -28,7 +33,8 @@ const PatientPage = () => {
         <div key={e.id}>
           {e.date} <i>{e.description}</i>
           <ul>
-            {e.diagnosisCodes?.map((c: string) => <li key={c}>{c}</li>)}
+            {e.diagnosisCodes?.map((c: string) =>
+              <li key={c}>{c} {diagnoses?.find((d) => d.code === c)?.name}</li>)}
           </ul>
         </div>)}
     </div>
