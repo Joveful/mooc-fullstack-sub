314ef480c7f26339f70a82c073a22c7d1838eeaa
Jori Vehkakoski <j.vehkakoski@gmail.com>
2025-08-17 01:18:57 +0300

    get, get by id, post

--- part13/bloglist/index.js
+++ part13/bloglist/index.js
@@ -1,22 +1,68 @@
 import dotenv from 'dotenv';
-import { QueryTypes, Sequelize } from 'sequelize';
+import { DataTypes, Model, QueryTypes, Sequelize } from 'sequelize';
+import express from 'express';
+const app = express();
+
 dotenv.config();
 
 const sequelize = new Sequelize(process.env.DATABASE_URL);
 
-const main = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-    const blogs = await sequelize.query("SELECT * FROM blogs", {
-      type:
-        QueryTypes.SELECT
-    });
-    console.log(blogs);
-    sequelize.close();
-  } catch (error) {
-    console.error('Unable to connect to server:', error);
+class Blog extends Model { }
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER
   }
-}
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+});
 
-main();
+Blog.sync();
+
+app.get('/api/blogs', async (req, res) => {
+  const blogs = await Blog.findAll();
+  res.json(blogs);
+
+});
+
+app.post('/api/blogs', async (req, res) => {
+  console.log(req);
+  try {
+    const blog = await Blog.create(req.body);
+    res.json(blog);
+  } catch (error) {
+    return res.status(400).json({ error });
+  }
+});
+
+app.get('/api/blogs/:id', async (req, res) => {
+  const blog = await Blog.findByPk(req.params.id);
+  if (blog) {
+    res.json(blog);
+  } else {
+    res.status(400).end();
+  }
+});
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
